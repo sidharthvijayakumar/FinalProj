5f7cd479ea942b703eefbe735c5b789f051e9c75
DELL <DELL@192.168.1.11>
2020-09-28 11:02:56 +0530

    First commit

---
+++ github.com/sidharthvijayakumar/FinalProj.git/Final/src/main/java/com/infy/model/Final/App.java
@@ -0,0 +1,13 @@
+package com.infy.model.Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
